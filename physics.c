84e55939725a3bf2f987f5dc69ffafd3bf1fe901
Fake Face <54221093+Fake-Face@users.noreply.github.com>
2021-01-07 23:49:12 +0100

    Update physics.c

--- physics.c
+++ physics.c
@@ -1,4 +1,3 @@
-﻿
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -12,16 +11,14 @@ Ball Ball_set(Vec2 position)
     ball.position = position;
     ball.velocity = Vec2_set(0.f, 0.f);
     ball.mass = 0.5f;
-    ball.friction = 1.0f;
+    ball.friction = 3.f;
 
     return ball;
 }
 
 int Ball_connect(Ball *ball1, Ball *ball2, float length)
 {
-    if ((ball1 == ball2)
-        || (ball1->nbSprings >= MAX_EDGES)
-        || (ball2->nbSprings >= MAX_EDGES))
+    if ((ball1 == ball2) || (ball1->nbSprings >= MAX_EDGES)|| (ball2->nbSprings >= MAX_EDGES))
         return EXIT_FAILURE;
 
     ball1->springs[ball1->nbSprings].other = ball2;
@@ -77,65 +74,67 @@ int Ball_deconnect(Ball *ball1, Ball *ball2)
 
 void Ball_updateVelocity(Ball *ball, float timeStep)
 {
-    //Données de la balle
-      float mass = ball->mass;
-      float friction = ball->friction;
-      Vec2 velocity = ball->velocity;
-      Vec2 position = ball->position;
-      Vec2 other;
-      int nb_springs = ball->nbSprings;
-      //***************************Calcul des forces appliqués****************************//
-      // Le poids
-      Vec2 gravity = Vec2_set(0, -9.81);
-      Vec2 weight = Vec2_scale(gravity, mass);
+  //Données de la balle
+  float mass = ball->mass;
+  float friction = ball->friction;
+  Vec2 velocity = ball->velocity;
+  Vec2 position = ball->position;
+  Vec2 other;
+  int nb_springs = ball->nbSprings;
+  //***************************Calcul des forces appliqués****************************//
+  // Le poids
+  Vec2 gravity = Vec2_set(0, -9.81);
+  Vec2 weight = Vec2_scale(gravity, mass);
 
-      // La friction
-      Vec2 Friction = Vec2_scale(velocity, friction * -1);
-      Vec2 Rappel = Vec2_set(0.f, 0.f);
+  // La friction
+  Vec2 Friction = Vec2_scale(velocity, friction * -1);
+  Vec2 Rappel = Vec2_set(0.f, 0.f);
 
-      // Le rappel en fonction du nombre de balles ratachés
-      for (int i = 0; i < nb_springs; i++) {
-        other = ball->springs[i].other->position;
-        Vec2 vector_BalleAttach = Vec2_sub(other, position);
-        float l = Vec2_norm(vector_BalleAttach);
-        float l_0 = 1.f; // Longueur ressort au repos
-        float k = 300; // Raideur du ressort
-        Vec2 vector_I = Vec2_normalize(vector_BalleAttach);
-        Rappel = Vec2_add(Rappel, Vec2_scale(vector_I, k*(l-l_0)));
-      }
+  // Le rappel en fonction du nombre de balles ratachés
+  for (int i = 0; i < nb_springs; i++) {
+    other = ball->springs[i].other->position;
+    Vec2 vector_BalleAttach = Vec2_sub(other, position);
+    float l = Vec2_norm(vector_BalleAttach);
+    float l_0 = 1.f; // Longueur ressort au repos
+    float k = 300; // Raideur du ressort
+    Vec2 vector_I = Vec2_normalize(vector_BalleAttach);
+    Rappel = Vec2_add(Rappel, Vec2_scale(vector_I, k*(l-l_0)));
+  }
 
-      // La somme des force
-      Vec2 somme_forces = Vec2_add(weight, Friction);
-      somme_forces = Vec2_add(somme_forces, Rappel);
+  // La somme des force
+  Vec2 somme_forces = Vec2_add(weight, Friction);
+  somme_forces = Vec2_add(somme_forces, Rappel);
 
-      //*************************Calcule de la vitesse associé à la balle*******************//
-      //Calcul de l'acceleration de la balle
-      Vec2 acceleration = Vec2_scale(somme_forces, 1/mass);
+  //******************* Calcule de la vitesse associé à la balle *******************//
 
-      //Calcul de la vitesse de la balle
-      Vec2 Velocity = Vec2_add(velocity, Vec2_scale(acceleration, timeStep));
+  // Calcul de l'accéleration de la balle
+  Vec2 acceleration = Vec2_scale(somme_forces, 1/mass);
 
-      //Update de la balle
-      ball->velocity = Velocity;
+  // Calcul de la vitesse de la balle
+  Vec2 Velocity = Vec2_add(velocity, Vec2_scale(acceleration, timeStep));
+
+  // Update de la balle
+  ball->velocity = Velocity;
+
 }
 
 void Ball_updatePosition(Ball *ball, float timeStep)
 {
-    Ball_updateVelocity(ball, timeStep);
+  Ball_updateVelocity(ball, timeStep);
 
-    //Données de la balle
-    Vec2 velocity = ball->velocity;
-    Vec2 position = ball->position;
+  // Données de la balle
+  Vec2 velocity = ball->velocity;
+  Vec2 position = ball->position;
 
-    //Calcul de la position de la balle
-    Vec2 Position = Vec2_add(position, Vec2_scale(velocity, timeStep));
+  // Calcul de la position de la balle
+  Vec2 Position = Vec2_add(position, Vec2_scale(velocity, timeStep));
 
-    //Detection de colision avec le sol
-    if(Position.y < 1){
+  // Détection de colisions avec le sol
+  if(Position.y < 1){
   	ball->velocity.y *= -1;
-  	Position.y = 1;
-    }
+  	Position.y = 1.f;
+  }
 
-    //Update de la balle
-    ball->position = Position;
+  // Update de la balle
+  ball->position = Position;
 }
